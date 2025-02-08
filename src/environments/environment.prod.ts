07d1de201bd499f6731e6d657f455e8753615037
Deni Chan <deni.chan@mail.com>
2025-02-08 19:41:04 +0200

    ws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://dc-dictionary-api-26fa33a06afa.herokuapp.com',
-    wsUrl: 'ws://dc-dictionary-api-26fa33a06afa.herokuapp.com',
+    wsUrl: 'wss://dc-dictionary-api-26fa33a06afa.herokuapp.com',
 };
 
 
